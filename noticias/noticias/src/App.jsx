9168f17da4655f14e5cc60bcf023df83a0a05668
hannibal <hannibal.rabasso11@gmail.com>
2023-10-10 13:36:16 +0200

    ancla decente

---
+++ noticias/noticias/src/App.jsx
@@ -0,0 +1,13 @@
+
+import NewsBoard from './NewsBoard'; // Asegúrate de que la ruta sea correcta
+
+const App = () => {
+  return (
+    <div className="App">
+      <NewsBoard />
+      {/* Resto de tu contenido de la aplicación */}
+    </div>
+  );
+};
+
+export default App;
